4a7f53e4a0e272384e4d6ba5a58415f99245c9a3
mishka3g_swurg <102925565+mishka3g@users.noreply.github.com>
2026-05-17 15:04:18 +0300

    add files realna

---
+++ js/search.js
@@ -0,0 +1,14 @@
+const search = function () {
+  const input = document.querySelector('.search-block > input');
+  const searchBtn = document.querySelector('.search-block > buton');
+
+  // input.addEventListener('input', (event) => {
+  //   console.log(event.target.value);
+  // });
+  input.addEventListener('click', (event) => {
+    console.log(input.value);
+  });
+
+}
+
+search();
